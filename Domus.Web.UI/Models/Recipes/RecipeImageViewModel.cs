46d68ca88836f4bc5add23cccbc68281be14b129
Josh Rolstad <jrolstad@gmail.com>
2012-01-27 22:25:21 -0800

    Adding missing files

---
+++ Domus.Web.UI/Models/Recipes/RecipeImageViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Domus.Web.UI.Models.Recipes
+{
+    public class RecipeImageViewModel
+    {
+        public string RecipeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double Top { get; set; }
+
+        public double Left { get; set; }
+
+        public double Right { get; set; }
+
+        public double Bottom { get; set; }
+    }
+}
