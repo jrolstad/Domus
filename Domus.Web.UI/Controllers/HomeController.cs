f9a72a5f63d7fcb1f2db0462dc6b20e765127a9f
Josh Rolstad <jrolstad@gmail.com>
2012-01-27 22:52:59 -0800

    adding method to read logs

--- Domus.Web.UI/Controllers/HomeController.cs
+++ Domus.Web.UI/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Rolstad.Extensions;
+using log4net;
+using log4net.Appender;
 
 namespace Domus.Web.UI.Controllers
 {
@@ -14,6 +20,28 @@ namespace Domus.Web.UI.Controllers
         public ViewResult Index()
         {
             return View();
+        }
+
+        public ContentResult Logs()
+        {
+            var logs = LogManager
+                .GetRepository()
+                .GetAppenders()
+                .Where(a => a is FileAppender)
+                .Cast<FileAppender>()
+                .Select(a => a.File);
+
+            var logbuilder = new StringBuilder();
+            logs.Each(l=>
+                          {
+                              var newFilename = Guid.NewGuid().ToString();
+                              System.IO.File.Copy(l, newFilename);
+
+                              var file = System.IO.File.ReadAllLines(newFilename);
+                              file.Each(f => logbuilder.AppendLine(f));
+                          });
+
+            return Content(logbuilder.ToString());
         }
 
     }
