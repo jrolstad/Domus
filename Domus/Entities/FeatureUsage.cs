e670de40178c5939e6e237c3a97d40fa93c2e590
Josh Rolstad <jrolstad@gmail.com>
2012-11-21 13:16:49 -0800

    Adding Feature usage entity

---
+++ Domus/Entities/FeatureUsage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Directus.SimpleDb.Attributes;
+
+namespace Domus.Entities
+{
+    [Serializable]
+    [DomainName("Domus_FeatureUsage")]
+    public class FeatureUsage
+    {
+        [Key]
+        public string FeatureUsageId { get; set; }
+
+        public string FeatureName { get; set; }
+
+        public DateTime UsedAt { get; set; }
+
+        public string UsedBy { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
