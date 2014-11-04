7ccd866bcb3a9a34079eab0d03154605b676e71c
Josh Rolstad <jrolstad@gmail.com>
2014-11-03 21:01:22 -0800

    Initial commit to create new application

---
+++ domus.mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(domus.mvc.Startup))]
+namespace domus.mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
