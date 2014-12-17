d6af8b5ab5d20c0e8f70a5b270528b0865cedd3e
Josh Rolstad <jrolstad@gmail.com>
2014-12-17 05:51:22 -0800

    Adding models for EF

---
+++ domus.data/models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace domus.data.models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public string LastUpdatedBy { get; set; }
+    }
+}
---
+++ domus.data/models/Recipe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace domus.data.models
+{
+    public class Recipe
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        public virtual string ExternalRecipeId { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual int? Servings { get; set; }
+
+        public virtual int? Rating { get; set; }
+
+        public virtual int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public virtual string Ingredients { get; set; }
+
+        public virtual string Directions { get; set; }
+
+        public virtual string Source { get; set; }
+
+        public virtual bool IsDeleted { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public string LastUpdatedBy { get; set; }
+    }
+}
