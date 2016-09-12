1fef7f5b244ec2e95791d7c8fe568c1492d393d3
Josh Rolstad <jrolstad@gmail.com>
2016-09-12 09:13:22 -0700

    Adding code to export recipes as markdown

--- domus.markdown_exporter/Program.cs
+++ domus.markdown_exporter/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Domus.Entities;
 using Domus.Providers.Cacheing;
@@ -14,7 +16,77 @@ namespace domus.markdown_exporter
     {
         static void Main(string[] args)
         {
-            var recipeRepository = new AmazonSimpleDbRecipeProvider("AKIAIPPUKG4ROEJABARA", "u2M5frbU506uI3gQtwil8bWzdiam0bDYx0BZAgpB", new InMemoryCache());
+            ExportRecipes();
+
+            //ExtractCategories();
+        }
+
+        private static void ExtractCategories()
+        {
+            var categoryRepository = new AmazonSimpleDbCategoryProvider("AKIAIPPUKG4ROEJABARA",
+                "u2M5frbU506uI3gQtwil8bWzdiam0bDYx0BZAgpB", new InMemoryCache());
+
+            var categories = categoryRepository.Get().OrderBy(c => c.Description);
+
+            var categoryLinks = CreateCategoryLinks(categories);
+            File.WriteAllLines(@"D:\github\jrolstad\jrolstad.github.io\categories.txt", categoryLinks);
+
+            CreateCategoryFolders(categories);
+        }
+
+        private static void CreateCategoryFolders(IOrderedEnumerable<Category> categories)
+        {
+            foreach (var category in categories)
+            {
+                var name = ToCamelCase(category.Description);
+
+                var path = Path.Combine(@"D:\github\jrolstad\jrolstad.github.io\categories", name);
+
+                Directory.CreateDirectory(path);
+
+                var indexPage = Path.Combine(path, "index.html");
+                File.WriteAllText(indexPage, string.Format(@"---
+layout: categorypage
+category: {0}
+---
+", name));
+            }
+        }
+
+        private static List<string> CreateCategoryLinks(IOrderedEnumerable<Category> categories)
+        {
+            var categoryLinks = new List<string>();
+            foreach (var category in categories)
+            {
+                var categoryCased = ToCamelCase(category.Description);
+                var categoryLine =
+                    "<div class=\"categories-title\"><a class=\"sidebar-nav-item\" href=\"/categories/{0}\">{0}</a></div>";
+                var formattedLine = string.Format(categoryLine, categoryCased);
+                categoryLinks.Add(formattedLine);
+            }
+            return categoryLinks;
+        }
+
+        public static string ToCamelCase(string the_string)
+        {
+            the_string = ToPascalCase(the_string);
+            return the_string.Substring(0, 1).ToLower() +
+                the_string.Substring(1);
+        }
+
+        public static string ToPascalCase(string the_string)
+        {
+            TextInfo info = Thread.CurrentThread.CurrentCulture.TextInfo;
+            the_string = info.ToTitleCase(the_string);
+            string[] parts = the_string.Split(new char[] { },
+                StringSplitOptions.RemoveEmptyEntries);
+            string result = String.Join(String.Empty, parts);
+            return result;
+        }
+
+        private static void ExportRecipes()
+        {
+            var recipeRepository = new AmazonSimpleDbRecipeProvider("AKIAIPPUKG4ROEJABARA","u2M5frbU506uI3gQtwil8bWzdiam0bDYx0BZAgpB", new InMemoryCache());
 
             var recipes = recipeRepository.Get();
 
@@ -68,7 +140,8 @@ rating: {2}
             var source = GetSource(recipe);
             var ingredients = GetIngredients(recipe);
             var directions = GetDirections(recipe);
-            return string.Format(recipeFormat,recipe.Name,recipe.Category,recipe.Rating,servings,ingredients,directions,source);
+            var category = ToCamelCase(recipe.Category);
+            return string.Format(recipeFormat,recipe.Name,category,recipe.Rating,servings,ingredients,directions,source);
         }
 
         private static string GetDirections(Recipe recipe)
@@ -111,8 +184,6 @@ rating: {2}
             var directions = string.Join(Environment.NewLine, formattedDirectionLines);
             return directions;
         }
-
-
 
         private static string GetIngredients(Recipe recipe)
         {
